cf48d5dc13cfc7a4b7135dbb62f495343a8873ec
Prathamesh Ghorpade <prathamesh@dglide.com>
2024-12-26 17:59:04 +0530

    `Deleted file: uniqu_names.py, contents written to /dev/null`

--- uniqu_names.py
+++
@@ -1,9 +0,0 @@
-input_file = 'C:/Users/ghorp/Documents/GitHub/dglide_fix/all-mui-components.txt'
-
-with open(input_file, 'r') as file:
-    lines = file.readlines()
-
-unique_lines = set(dict.fromkeys(lines))
-
-with open(input_file, 'w') as file:
-    file.writelines(unique_lines)
